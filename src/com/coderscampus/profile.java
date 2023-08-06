5b72c4aadef5d0e41608c60951d9b7bcb77164fa
Blackrose04 <140462912+Blackrose04@users.noreply.github.com>
2023-08-06 16:18:28 -0400

    updated profile date

--- src/com/coderscampus/profile.java
+++ src/com/coderscampus/profile.java
@@ -1,5 +1,22 @@
-package com.coderscampus;
+package com.coderscampus.dommain;
 
 public class profile {
+	private Integer id;
+	private String name;
+	private String Url;
+	private Localdate createDate;
+	
+	public Localdate getcreatedDate() {
+		return createdDate;
+	}
+	
+	public Integer getId() {
+		return id; 
+	}
+	public void setId (Integer id) {
+		this.id = id;
+	}
+	
+	}
 
 }
